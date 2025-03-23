20841149c12c8442d4a6a5b9bc4cb496e6291976
Selviaokt <selviashlh@gmail.com>
2025-03-24 06:57:09 +0700

    modul1

--- praktikum3/tugas_session/proses_login.php
+++ praktikum3/tugas_session/proses_login.php
@@ -1,4 +1,4 @@
-<?php session_start(); $user = "selviacantik"; $pass = "123"; if ($_POST['username'] == $user && $_POST['password'] == $pass) { $_SESSION['username'] = $user; header("Location: dashboard.php"); } else { echo "Login Gagal!"; } ?>
+<?php session_start(); $user = "selviaokt"; $pass = "123"; if ($_POST['username'] == $user && $_POST['password'] == $pass) { $_SESSION['username'] = $user; header("Location: dashboard.php"); } else { echo "Login Gagal!"; } ?>
 
 
 
